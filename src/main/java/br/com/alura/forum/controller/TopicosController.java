b9a64a42b8f798ca0566465767ec03a00d5f33ee
vitor batista <vitor.shofar@gmail.com>
2020-02-23 18:32:19 -0300

    add suporte a ordenacao na busca de topicos

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,9 +44,10 @@ public class TopicosController {
 	@GetMapping
 	public Page<TopicoDto> listar(String nomeCurso, 
 			@RequestParam int pagina,
-			@RequestParam int quantidade){
+			@RequestParam int quantidade,
+			@RequestParam String ordenacao){
 		
-		Pageable paginacao =  PageRequest.of(pagina, quantidade);
+		Pageable paginacao =  PageRequest.of(pagina, quantidade, Direction.ASC, ordenacao);
 
 		if(nomeCurso == null) {
 			Page<Topico> topicos = topicoRepository.findAll(paginacao);
